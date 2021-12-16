8de77eee3afa5bae8e199b171d9814197ec2219d
תומי ירמיה <tommyirmia@gmail.com>
2021-12-16 15:02:37 +0200

    nothing

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import { Route, Switch } from 'react-router'
+import { Switch } from 'react-router'
 import routes from './routes'
-import {AppHeader} from './cmps/app-header.jsx'
+import { AppHeader } from './cmps/app-header.jsx'
 
-export const RootCmp =() => {
+export const RootCmp = () => {
     return (
         <div className="app">
             <AppHeader />
             <main>
-            <Switch>
-                        {routes.map(route => <Route key={route.path} component={route.component} path={route.path} />)}
-                    </Switch>
+                <Switch>
+                    {routes.map(route => <Route key={route.path} component={route.component} path={route.path} />)}
+                </Switch>
             </main>
         </div>
     )
