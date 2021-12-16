dc74555c5aa0e67ec50ccb40c7d73a16c9e1e17e
תומי ירמיה <tommyirmia@gmail.com>
2021-12-16 18:23:41 +0200

    fixed bug

--- src/views/home-page.jsx
+++ src/views/home-page.jsx
@@ -18,7 +18,7 @@ export const HomePage = () => {
 
     return (
         <section className="home-page">
-            <div className="bars-list-container">
+            {bars && <div className="bars-list-container">
                 <div className="list-header flex space-between">
                     <h1>מומלצים עבורי</h1>
                     <h2>מקומות נוספים</h2>
@@ -27,6 +27,7 @@ export const HomePage = () => {
                     {bars.map(bar => <BarPreview bar={bar} />)}
                 </div>
             </div>
+            }
             <div className="around-list"></div>
             <div className="visited-list"></div>
         </section>
